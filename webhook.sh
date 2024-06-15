b82b8ec3c45ceea3d1fdfad5911f66fc3d192ad4
kyusikko <kokyusik91@naver.com>
2024-06-15 19:11:26 +0900

    🔨 배포 완료시 알람오는 웹훅 수정

--- webhook.sh
+++ webhook.sh
@@ -4,7 +4,7 @@ send_slack_message() {
     local slack_url="$1"
     local status="$2"
     local current_time=$(TZ=Asia/Seoul date +"%Y-%m-%d %H:%M:%S")
-    local message="배포가 $status 하였습니다. 🥳 - 현재 시간: $current_time"
+    local message="큐니버스 배포가 $status 하였습니다. 🥳 - 현재 시간: $current_time"
     curl -X POST --data-urlencode "payload={\"text\": \"$message\", \"icon_emoji\": \":earth_asia:\"}" $slack_url
 }
 send_slack_message $1 $2 
