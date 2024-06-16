be80e0fa77b336c7514b708459775a6732e2e609
kyusikko <kokyusik91@naver.com>
2024-06-16 14:22:06 +0900

    ✨ 프로젝트내에서 사용하는 변수들 파일 별개 추출

---
+++ app/constants/fashion.ts
@@ -0,0 +1,32 @@
+export const PANTS = [
+  '/zard.png',
+  '/kitty.png',
+  '/athlete.png',
+  '/jeans.png',
+  '/watthe.png',
+];
+
+export const CLOTHES = [
+  '/crown.png',
+  '/zombie.png',
+  '/stussy2.png',
+  '/stuussy.png',
+  '/supreme.png',
+  '/supreme2.png',
+  '/dice.png',
+  '/8ball.png',
+];
+
+export const MANHEAD = [
+  '👦🏼',
+  '👶🏻',
+  '🧒🏻',
+  '👩🏼',
+  '👨🏼',
+  '🧔🏻‍♂️',
+  '👩🏼‍🦱',
+  '👩🏽‍🦰',
+  '👨🏼‍🦰',
+  '👱🏼‍♀️',
+  '🧓🏼',
+];
---
+++ app/constants/skillset.ts
@@ -0,0 +1,56 @@
+export const SKILL = [
+  {
+    value: 'react',
+    label: 'REACT',
+    color: 'text-blue-400',
+    url: '/react2.svg',
+  },
+  {
+    value: 'next',
+    label: 'NEXT.JS',
+    color: 'text-gray-500',
+    url: '/nextjs.svg',
+  },
+  {
+    value: 'tanstack',
+    label: 'Tanstack',
+    color: 'text-red-500',
+    url: '/tanstack.webp',
+  },
+  {
+    value: 'express',
+    label: 'EXPRESS.JS',
+    color: 'text-green-500',
+    url: '/Expressjs.png',
+  },
+  {
+    value: 'vue',
+    label: 'VUE.JS',
+    color: 'text-green-400',
+    url: '/vue.png',
+  },
+  {
+    value: 'svelte',
+    label: 'SVELETE',
+    color: 'text-orange-400',
+    url: '/svelte.png',
+  },
+  {
+    value: 'nest',
+    label: 'NEST.JS',
+    color: 'text-red-400',
+    url: '/nest.svg',
+  },
+  {
+    value: 'webpack',
+    label: 'WEBPACK',
+    color: 'text-zinc-200',
+    url: '/webpack.svg',
+  },
+  {
+    value: 'vite',
+    label: 'VITE',
+    color: 'text-purple-400',
+    url: '/vite.svg',
+  },
+];
