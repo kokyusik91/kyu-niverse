dfb2535c09e1005ff32e52c57adffddad8ce2268
kyusikko <kokyusik91@naver.com>
2024-06-16 17:05:00 +0900

    💄 스타일 수정

--- app/components/slidersmall.tsx
+++ app/components/slidersmall.tsx
@@ -1,42 +1,7 @@
 'use client';
 
 import React, { useMemo, useState } from 'react';
-
-const fakedata = [
-  {
-    title: '고질라-1',
-    content: '웹표준, 접근성/크로스브라우징, 반응형 웹, SEO, SSR',
-  },
-  {
-    title: '테일윈드-2',
-    content:
-      '사람이 원하는 요구사항을 어떠한 계속적으로 변하는 값, 즉 데이터의 형태로 만들어주는 것을 의미합니다.',
-  },
-  {
-    title: '파라마라-3',
-    content:
-      'If you are using a static host like Nginx, you can configure rewrites from incoming requests to the correct files:',
-  },
-  {
-    title: '스벨트-4',
-    content: 'Authorization is the process of granting an integratio',
-  },
-  {
-    title: '뷰제이에스-5',
-    content:
-      '론트엔드 개발자의 능력인 기획의 요구사항을 String, Array, Object와 같은 형태의 값으로 만들어내는 과정을 하는데 있어서',
-  },
-  {
-    title: '리액트-6',
-    content:
-      'For example, the below query limits the response to entries where the "Task completed" checkbox property value is true:',
-  },
-  {
-    title: '사마사마-7',
-    content:
-      'For example, the below query limits the response to entries where the "Task completed" checkbox property value is true:',
-  },
-];
+import Image from 'next/image';
 
 type CardData = {
   title: string;
@@ -78,7 +43,7 @@ function Card({
     >
       {/* <h1>{card.title} 🧐</h1>
       <p>{card.content}</p> */}
-      <img className='w-full h-full' src={card} alt={card} />
+      <Image fill src={card} alt={card} />
     </div>
   );
 }
