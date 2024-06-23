2ce861a967491d508295c090b84653d900745b15
kyusikko <kokyusik91@naver.com>
2024-06-23 13:08:59 +0900

    ♻️ 마이 클로젯 슬라이드 컴포넌트 수정

--- app/components/slider/headSlider.tsx
+++ app/components/slider/headSlider.tsx
@@ -1,6 +1,7 @@
-'use client';
+"use client";
 
-import React, { useMemo, useState } from 'react';
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import React, { useMemo, useState } from "react";
 
 export default function HeadSlider({ data }: { data: string[] }) {
   const [activeIndex, setActiveIndex] = useState(5);
@@ -24,56 +25,49 @@ export default function HeadSlider({ data }: { data: string[] }) {
 
   const amount = useMemo(() => data?.length, [data?.length]);
 
+  const changeStyle = (index: number) =>
+    distanceFromActiveIndex(index) !== 0
+      ? {
+          transform: `translateX(${distanceFromActiveIndex(index) * 100}%)`,
+          filter: "blur(1px)",
+          zIndex: `${10 - Math.abs(distanceFromActiveIndex(index))}`,
+          opacity: 0,
+        }
+      : {
+          zIndex: 10,
+          filter: "none",
+          opacity: view ? 1 : 0,
+        };
+
   return (
-    <ul className='mb-4 relative w-full h-[80px] z-90'>
+    <ul className="h-1/10 relative flex w-full items-center">
       {data.map((man, index) => (
         <li
+          className="absolute left-1/2 -translate-x-1/2 transform text-justify text-7xl transition-all duration-500"
           key={index}
-          style={
-            distanceFromActiveIndex(index) !== 0
-              ? {
-                  transform: `translateX(${
-                    distanceFromActiveIndex(index) * 100
-                  }%)`,
-                  filter: 'blur(1px)',
-                  zIndex: `${10 - Math.abs(distanceFromActiveIndex(index))}`,
-                  opacity: 0,
-                }
-              : {
-                  zIndex: 10,
-                  filter: 'none',
-                  opacity: view ? 1 : 0,
-                }
-          }
-          className='head-item'
+          style={changeStyle(index)}
         >
           {man}
         </li>
       ))}
-      {view && (
-        <button
-          className='absolute top-2 left-10 z-10'
-          onClick={handleClickPrev}
-          disabled={activeIndex === 0}
-        >
-          {activeIndex === 0 ? '' : '⬅️'}
+      {/* 좌측 버튼 */}
+      {view && activeIndex !== 0 && (
+        <button className="absolute left-10 z-10" onClick={handleClickPrev}>
+          <ChevronLeft size={20} color="black" />
+        </button>
+      )}
+      {/* 우측 버튼 */}
+      {view && amount !== activeIndex + 1 && (
+        <button className="absolute right-10 z-10" onClick={handleClickNext}>
+          <ChevronRight size={20} color="black" />
         </button>
       )}
       <button
         onClick={toggleView}
-        className='absolute left-1/2 transform -translate-x-1/2 z-10'
+        className="absolute left-1/2 z-10 -translate-x-1/2 -translate-y-[50px] transform"
       >
-        {view ? '❌' : '👨🏼'}
+        {view ? "❌" : "👨🏼"}
       </button>
-      {view && (
-        <button
-          className='absolute top-2 right-10 z-10'
-          onClick={handleClickNext}
-          disabled={amount === activeIndex + 1}
-        >
-          {amount === activeIndex + 1 ? '🙅🏼‍♀️' : '➡️'}
-        </button>
-      )}
     </ul>
   );
 }
